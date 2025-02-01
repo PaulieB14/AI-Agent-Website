ff385f5e44ebfcfb12b30bf1c1d46b5feb61704f
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2025-02-01 14:22:59 -0500

    fix tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,14 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        // Custom screen sizes (optional)
+        xs: "480px", // For extra small screens (e.g., mobile)
+        sm: "640px", // Small screens (tablets)
+        md: "768px", // Medium screens (larger tablets and some laptops)
+        lg: "1024px", // Larger screens (desktops)
+        xl: "1280px", // Extra large screens (big desktops)
+      },
     },
   },
   plugins: [],
